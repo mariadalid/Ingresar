979a043d4a1d6304a5d4529f4b19f2b9e44c70de
mariadalid <20243rd014@utez.edu.mx>
2025-05-13 21:05:35 -0600

    Iniciar sesión

---
+++ Ingresar.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Ingresar {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static String obtnerCadena(String mensaje){
+        String cadenaTemporal;
+        System.out.print(mensaje + ": ");
+        cadenaTemporal = scanner.nextLine();
+        return cadenaTemporal;
+    }
+    //Imprimir tu los meses del año 
+    public static void main (String[] args) {
+        String nombreUsuario = "Adalid";
+        String contrasenaUsuario = "32514";
+        String usuarioIngresado = obtnerCadena("Ingresa tu usuario");
+        String contrasenaIngresada = obtnerCadena("Ingresa tu contraseña");
+
+        if((nombreUsuario.equals(usuarioIngresado)) && (contrasenaIngresada.equals(contrasenaUsuario))){
+            System.out.println("Inicio de sesión, exitoso!");
+        } else {
+            System.out.println("Usuario o contraseña incorrectos");
+        }
+    }
+}
